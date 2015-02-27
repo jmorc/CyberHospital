c4668bf141d7f12076f3222dd177bb3a75faedc6
jmorc <jeffcaves@gmail.com>
2015-02-27 14:03:25 -0800

    Added Clinics resource

---
+++ app/controllers/clinics_controller.rb
@@ -0,0 +1,47 @@
+class ClinicsController < ApplicationController
+  before_action :set_clinic, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @clinics = Clinic.all
+    respond_with(@clinics)
+  end
+
+  def show
+    respond_with(@clinic)
+  end
+
+  def new
+    @clinic = Clinic.new
+    respond_with(@clinic)
+  end
+
+  def edit
+  end
+
+  def create
+    @clinic = Clinic.new(clinic_params)
+    @clinic.save
+    respond_with(@clinic)
+  end
+
+  def update
+    @clinic.update(clinic_params)
+    respond_with(@clinic)
+  end
+
+  def destroy
+    @clinic.destroy
+    respond_with(@clinic)
+  end
+
+  private
+    def set_clinic
+      @clinic = Clinic.find(params[:id])
+    end
+
+    def clinic_params
+      params.require(:clinic).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :clinics
+
   root to: "static_pages#root"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
