977a0ac072312d94eededbded90cbbb241cf1b5e
jmorc <jeffcaves@gmail.com>
2015-03-05 20:34:15 -0800

    added routes and buttons for edit, new, destroying ptx data

---
+++ app/controllers/patient_data_controller.rb
@@ -0,0 +1,19 @@
+class PatientDataController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :patients, except: [:new, :edit, :index]
+
+  resources :patient_data, except: [:new, :edit, :index]
+
+  resources :patients, except: [:new, :edit, :index] do
+    resources :patient_data, only: [:new, :edit]
+  end
 
   resources :clinics do 
     resources :patients, only: [:new, :edit]
