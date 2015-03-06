d591a9910ee1a980c48fcbc2932adfc4548bd2c8
jmorc <jeffcaves@gmail.com>
2015-03-06 11:09:34 -0800

    Can edit patient data from patient show view

--- app/controllers/patient_data_controller.rb
+++ app/controllers/patient_data_controller.rb
@@ -3,6 +3,15 @@ class PatientDataController < ApplicationController
   end
 
   def update
+    @patient_data = PatientData.find(params[:id])
+    
+    if @patient_data.update(patient_data_params)
+      redirect_to @patient_data.patient
+    else
+      flash[:errors] = @patient_data.errors.full_messages
+      redirect_to @patient_data.patient
+    end
+
   end
 
   def destroy
@@ -15,5 +24,13 @@ class PatientDataController < ApplicationController
   end
 
   def edit
+    @patient_data = PatientData.find(params[:id]) 
   end
+
+  private
+
+  def patient_data_params
+    params.require(:patient_data).permit(:name, :value, :comment)
+  end
+
 end
