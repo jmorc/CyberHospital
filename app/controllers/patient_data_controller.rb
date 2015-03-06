1805f2d4d2a352efb785f4824539e2970e5839fc
jmorc <jeffcaves@gmail.com>
2015-03-06 11:29:34 -0800

    can add patient data from patient show view

--- app/controllers/patient_data_controller.rb
+++ app/controllers/patient_data_controller.rb
@@ -1,36 +1,54 @@
 class PatientDataController < ApplicationController
   def create
+    @patient = Patient.find(params[:patient_data][:patient_id])
+    @patient_data = @patient.patient_datas.new(patient_data_params)
+
+    if @patient_data.save!
+      flash[:notice] = 'New Patient Data Saved'
+      redirect_to @patient_data.patient
+    else
+      flash[:notice] = @patient_data.errors.full_messages
+      redirect_to @patient_data.patient
+    end
+
   end
 
   def update
     @patient_data = PatientData.find(params[:id])
     
     if @patient_data.update(patient_data_params)
+      flash[:notice] = 'Patient Data updated'
       redirect_to @patient_data.patient
     else
-      flash[:errors] = @patient_data.errors.full_messages
+      flash[:notice] = @patient_data.errors.full_messages
       redirect_to @patient_data.patient
     end
 
   end
 
   def destroy
+    @patient_data = PatientData.find(params[:id])
+    @patient_data.destroy
+    redirect_to @patient_data.patient
   end
 
   def show
   end
 
   def new
+    @patient = Patient.find(params[:patient_id])
+    @patient_data = PatientData.new
   end
 
   def edit
+    @patient = Patient.find(params[:patient_id])
     @patient_data = PatientData.find(params[:id]) 
   end
 
   private
 
   def patient_data_params
-    params.require(:patient_data).permit(:name, :value, :comment)
+    params.require(:patient_data).permit(:name, :value, :comment, :patient_id)
   end
 
 end
