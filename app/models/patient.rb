81ba2a894172730d7808f6b84e08524dba423126
jmorc <jeffcaves@gmail.com>
2015-03-09 18:59:48 -0700

    all views look good for student viewer

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ActiveRecord::Base
 	belongs_to :clinic
 	has_many :patient_datas
+
+	def viewable?
+		Time.now > self.viewable_time.to_time && (cannot? :update, @patient) 
+	end
 end
--- app/views/patients/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@patients) do |patient|
-  json.extract! patient, :id, :name, :summary, :temperature, :heart_rate
-  json.url patient_url(patient, format: :json)
-end
