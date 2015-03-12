ef321b1082c51d5c9683410a3b0d48f7a0d4a6de
jmorc <jeffcaves@gmail.com>
2015-03-11 20:44:11 -0700

    used more buttons, cleaned up views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :new, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   load_and_authorize_resource
 
